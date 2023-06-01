6c6b014753799301193196378641782f91c0ebc4
Renan Mischiatti <renan.mischiatti@hotmail.com>
2023-05-31 22:04:18 -0300

    começo estilo chat

--- resources/views/ajax/chat/openChat.blade.php
+++ resources/views/ajax/chat/openChat.blade.php
@@ -1,13 +1,18 @@
-<main class="w-100 h-100" id="conversa">
-    <header class="">
-        <div class="col-1 d-flex justify-center py-2">
-            <img src="{{$match->UserMatched->foto->first()->FotCaminho}}" class="rounded-circle" alt="">
+<main class="w-100 h-100 d-flex flex-column" id="conversa">
+    <header class="d-flex">
+        <div class="d-flex justify-center p-3 foto">
+            <img src="{{$match->UserMatched->foto->first()->FotCaminho}}" class="rounded-circle" alt=""> 
         </div>
-        <div class="col-11 d-flex justify-content-center flex-column">
+        <div class="col-11 d-flex justify-content-center flex-column conteudo">
             <b class="nome text-gray-800 dark:text-gray-200 ">{{$match->UserMatched->info->nome}}</b>
             <b class="text-success status">online</b>
         </div>
     </header>
+    <section id="chat" class="h-100" style="border: 1px solid red">
 
+    </section>
+    <section id="sendMensageArea">
+
+    </section>
 
 </main>  
